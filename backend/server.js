73eb79aabe12a0ddca215892257f7523f86590cb
Noel Paing Oak Soe <u6530183@au.edu>
2024-10-22 01:50:59 +0700

    init set up

---
+++ backend/server.js
@@ -0,0 +1,18 @@
+import express from 'express'
+import dotenv from 'dotenv'
+import connectDB from './dbconnect/db.js'
+
+dotenv.config()
+
+const PORT = process.env.PORT || 5000
+
+const app = express()
+
+app.use(express.json())
+
+// app.use('/api/auth', authRoutes)
+
+app.listen(PORT, () => {
+  connectDB()
+  console.log(`Server is running on port ${PORT}`)
+})
