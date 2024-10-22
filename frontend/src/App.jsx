3268a07cdf3f44fd9a9a1dea2f84d90b2571b6db
Noel Paing Oak Soe <u6530183@au.edu>
2024-10-23 02:55:38 +0700

    add frontend strcture

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,33 +1,35 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { Routes, Route, Navigate } from 'react-router-dom'
+import Home from './pages/home/Home'
+import Login from './pages/login/Login'
+import SignUp from './pages/signup/SignUp'
+import Chat from './pages/chat/Chat'
+import { useAuthContext } from './context/AuthContext'
+import { Toaster } from 'react-hot-toast'
 
 function App() {
-  const [count, setCount] = useState(0)
-
+  const { authUser } = useAuthContext()
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <Routes>
+        {/* <Route
+          path='/'
+          element={authUser ? <Home /> : <Navigate to={'/login'} />}
+        />
+        <Route
+          path='/login'
+          element={authUser ? <Navigate to='/' /> : <Login />}
+        />
+        <Route
+          path='/signup'
+          element={authUser ? <Navigate to='/' /> : <SignUp />}
+        /> */}
+        <Route path='/' element={<Home />} />
+        <Route path='/chat' element={<Chat />} />
+        <Route path='/login' element={<Login />} />
+        <Route path='/signup' element={<SignUp />} />
+      </Routes>
+      <Toaster />
     </>
   )
 }
---
+++ frontend/src/components/messages/Message.jsx
@@ -0,0 +1,30 @@
+import React from 'react'
+
+const Message = () => {
+  return (
+    <div className={`chat start`}>
+      <div className='chat-image avatar'>
+        <div className='w-10 rounded-full'>
+          {/* src={profilePic} */}
+          <img
+            alt='Tailwind CSS chat bubble component'
+            src='https://cdn0.iconfinder.com/data/icons/communication-line-10/24/account_profile_user_contact_person_avatar_placeholder-512.png'
+          />
+        </div>
+      </div>
+      <div
+        //   ${bubbleBgColor} ${shakeClass}
+        className={`chat-bubble text-white pb-2`}
+      >
+        {/* {message.message} */}
+        Hello world
+      </div>
+      <div className='chat-footer opacity-50 text-xs flex gap-1 items-center'>
+        {/* {formattedTime} */}
+        12:00 PM
+      </div>
+    </div>
+  )
+}
+
+export default Message
