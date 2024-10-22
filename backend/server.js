6c1b4417678d905b0b45ccc6b7cb59079afec1b6
Noel Paing Oak Soe <u6530183@au.edu>
2024-10-23 00:26:46 +0700

    add message, user endpoints

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,9 @@ import express from 'express'
 import dotenv from 'dotenv'
 import connectDB from './dbconnect/db.js'
 import authRoutes from './routes/auth.routes.js'
+import userRoutes from './routes/user.routes.js'
+import messageRoutes from './routes/message.routes.js'
+import cookieParser from 'cookie-parser'
 
 dotenv.config()
 
@@ -10,8 +13,11 @@ const PORT = process.env.PORT || 5000
 const app = express()
 
 app.use(express.json())
+app.use(cookieParser())
 
 app.use('/api/auth', authRoutes)
+app.use('/api/users', userRoutes)
+app.use('/api/messages', messageRoutes)
 
 app.listen(PORT, () => {
   connectDB()
